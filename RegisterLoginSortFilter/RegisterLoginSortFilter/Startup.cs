8673e4a7bec192127b1c7e0e14a2287168918037
justasem <justasem@gmail.com>
2017-04-26 11:08:22 +0300

    Initial commit, project created

---
+++ RegisterLoginSortFilter/RegisterLoginSortFilter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RegisterLoginSortFilter.Startup))]
+namespace RegisterLoginSortFilter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
