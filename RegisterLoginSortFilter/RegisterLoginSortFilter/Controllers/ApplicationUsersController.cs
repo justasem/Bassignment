54f2b797acb0afd6a0562393af46efed475d75e9
justasem <justasem@gmail.com>
2017-04-26 16:53:15 +0300

    Added Sorting, Filtering and Paging for All Users page

--- RegisterLoginSortFilter/RegisterLoginSortFilter/Controllers/ApplicationUsersController.cs
+++ RegisterLoginSortFilter/RegisterLoginSortFilter/Controllers/ApplicationUsersController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using RegisterLoginSortFilter.Models;
+using PagedList;
 
 namespace RegisterLoginSortFilter.Controllers
 {
@@ -15,9 +16,65 @@ namespace RegisterLoginSortFilter.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: ApplicationUsers
-        public ActionResult Index()
+        public ViewResult Index(SortFilterPageParams parameters)
         {
-            return View(db.Users.ToList());
+
+            ViewBag.CurrentSort = parameters.sortOrder;
+            ViewBag.NameSortParm = string.IsNullOrEmpty(parameters.sortOrder) ? "name_desc" : "";
+            ViewBag.EmailSortParm = parameters.sortOrder == "Email" ? "email_desc" : "Email";
+
+            if (parameters.searchEmailString!= null)
+            {
+                parameters.page = 1;
+            }
+            else
+            {
+                parameters.searchEmailString = parameters.currentFilter;
+            }
+
+            ViewBag.CurrentFilter = parameters.searchEmailString;
+
+            if (parameters.searchNameString != null)
+            {
+                parameters.page = 1;
+            }
+            else
+            {
+                parameters.searchNameString = parameters.currentFilter;
+            }
+
+            ViewBag.CurrentFilter = parameters.searchNameString;
+
+            var users = from u in db.Users
+                        select u;
+
+            if (!string.IsNullOrEmpty(parameters.searchEmailString))
+            {
+                users = users.Where(u => u.Email.Contains(parameters.searchEmailString));
+            }
+            if (!string.IsNullOrEmpty(parameters.searchNameString))
+            {
+                users = users.Where(u => u.Name.Contains(parameters.searchNameString));
+            }
+
+            switch (parameters.sortOrder)
+            {
+                case "name_desc":
+                    users = users.OrderByDescending(u => u.Name);
+                    break;
+                case "Email":
+                    users = users.OrderBy(u => u.Email);
+                    break;
+                case "email_desc":
+                    users = users.OrderByDescending(u => u.Email);
+                    break;
+                default:
+                    users = users.OrderBy(u => u.Name);
+                    break;
+            }
+            int pageSize = 5;
+            int pageNumber = (parameters.page ?? 1);
+            return View(users.ToPagedList(pageNumber, pageSize));
         }
 
         protected override void Dispose(bool disposing)
---
+++ RegisterLoginSortFilter/RegisterLoginSortFilter/Models/SortFilterPageParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegisterLoginSortFilter.Models
+{
+    public class SortFilterPageParams
+    {
+        public string sortOrder { get; set; }
+        public string currentFilter { get; set; }
+        public string searchNameString { get; set; }
+        public string searchEmailString { get; set; }
+        public int? page { get; set; }
+    }
+}
