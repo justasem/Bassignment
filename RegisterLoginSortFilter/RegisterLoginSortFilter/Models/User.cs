ada25773b7be71e93602091a6f4e1b5ffcb984f5
justasem <justasem@gmail.com>
2017-04-26 12:03:57 +0300

    Added User model class

---
+++ RegisterLoginSortFilter/RegisterLoginSortFilter/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RegisterLoginSortFilter.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string AdditionalInfo { get; set; }
+
+    }
+
+    public class UserDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+    }
+
+}
